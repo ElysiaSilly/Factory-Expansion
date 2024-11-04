0cc93b11170d628b886f04c845189f76236df564
Elysia <86559966+ElysiaSilly@users.noreply.github.com>
2024-11-04 15:55:14 +0100

    coloured blocks helper (not working properly)

--- src/main/java/com/teamcitrus/factory_expansion/core/registry/FEBlocks.java
+++ src/main/java/com/teamcitrus/factory_expansion/core/registry/FEBlocks.java
@@ -4,11 +4,13 @@ import com.teamcitrus.factory_expansion.common.block.GirderBlock;
 import com.teamcitrus.factory_expansion.common.block.VariantBlock;
 import com.teamcitrus.factory_expansion.core.FactoExpa;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.*;
 import java.util.function.Supplier;
 
 public class FEBlocks {
@@ -24,6 +26,8 @@ public class FEBlocks {
     public static final Supplier<? extends Block> VANTA_ALLOY_BLOCK
             = registerBlockItem("vanta_alloy_block", () -> new Block(getProperties(Blocks.NETHERITE_BLOCK)));
 
+    public static final Dictionary<DyeColor, Supplier<? extends Block>> LAMPS
+            = registerColouredBlockItems("lamp", () -> new Block(getProperties(Blocks.NETHERITE_BLOCK)));
 
     public static final Supplier<? extends Block> GIRDER
             = registerBlockItem("girder", () -> new GirderBlock(getProperties(Blocks.NETHERITE_BLOCK)));
@@ -34,10 +38,24 @@ public class FEBlocks {
     // HELPERS
 
     // register block and item
-    private static Supplier<? extends Block> registerBlockItem(String resourceLocation, Supplier<? extends Block> blockType) {
-        var tempBlock = BLOCKS.register(resourceLocation, blockType);
+    private static Supplier<? extends Block> registerBlockItem(String id, Supplier<? extends Block> blockType) {
+        var tempBlock = BLOCKS.register(id, blockType);
         BLOCKITEMS.registerSimpleBlockItem(tempBlock);
         return tempBlock;
+    }
+
+    // HELP!!
+    // register colour set of block and item
+    private static Dictionary<DyeColor, Supplier<? extends Block>> registerColouredBlockItems(String id, Supplier<? extends Block> blockType) {
+
+        Dictionary<DyeColor, Supplier<? extends Block>> blocks = new Hashtable<>();
+
+        for(DyeColor colour : DyeColor.values()) {
+            id = String.format("%s_%s", colour.getName(), id);
+            Supplier<? extends Block> block = registerBlockItem(id, blockType);
+            blocks.put(colour, block);
+        }
+        return blocks;
     }
 
     @SuppressWarnings("unchecked")
